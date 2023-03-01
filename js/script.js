14d92cdd033a7e1b6502afd13890c5a8702493ed
Lukáš Lešák <lukas.kalna@seznam.cz>
2023-03-01 16:56:35 +0100

    jq hotovo

--- js/script.js
+++ js/script.js
@@ -4,9 +4,28 @@
         //     $(".forjqphotos").slideToggle(200)
         // });
 
-        $('.jq--show-more-btn').click(function($e) {
+        $('.jq--show-more-btn').click(function ($e) {
             $e.preventDefault();
             $(".forjqphotos").slideToggle(200);
         });
- });
+        
+        // $(".jq--show-more-btn").click(function(){
+        //     if($(".jq--show-more-btn").text("Zobrazit všechny výhody"))    //z elementu s class jq--image-hamburger vytáhne src
+        //     {
+        //         $($(".jq--show-more-btn").text("Zobrazit méně"));    //src se zm2n9 na crossMenu.png
+        //     }
+        //     else 
+        //     {
+        //         $($(".jq--show-more-btn").text("Zobrazit všechny výhody"));
+        //     }
+        // });
+
+
+        $(".jq--show-more-btn").click(function(){
+            $(".jq--show-more-btn").text() === 'Zobrazit všechny výhody' ? $(this).text('Zobrazit méně') : $(this).text('Zobrazit všechny výhody');
+        });
+
+
+        
+    });
 })(jQuery);
