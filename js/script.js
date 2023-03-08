d1c71b8ba6277fe450bdb93d6aac77168bf407e8
Lukáš Lešák <lukas.kalna@seznam.cz>
2023-03-08 19:23:20 +0100

    Done v1

--- js/script.js
+++ js/script.js
@@ -1,9 +1,5 @@
 (function ($) {
     $(function () {
-        // $(".jq--show-more-btn").click(function(){
-        //     $(".forjqphotos").slideToggle(200)
-        // });
-
         $('.jq--show-more-btn').click(function ($e) {
             $e.preventDefault();
             $(".forjqphotos").slideToggle(200);
